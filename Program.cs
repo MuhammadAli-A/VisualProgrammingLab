ac56054d102bf0b06aa1f9f572de066bf285f93b
MuhammadAli-A <233510@students.au.edu.pk>
2024-10-15 15:33:21 +0500

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int[] number = {4,2,6,1,2,3,-2,-1,0};
-foreach (int i in number)
-{
-    Console.WriteLine("{0}" ,i);
-
-}
