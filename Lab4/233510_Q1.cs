26b7f452bf06de64207f1db034f729c46c369d54
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-10 02:01:35 +0500

    Add files via upload

---
+++ Lab4/233510_Q1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize a two-dimensional array of integers
+            int[,] array = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+            // Call the method to print the array
+            Print2DArray(array);
+        }
+
+        // Method to print the elements of a two-dimensional array
+        static void Print2DArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
