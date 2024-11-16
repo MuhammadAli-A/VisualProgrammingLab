6415e0f3d41162ab8a5cfa77dbd2ffe0c1d5e3e2
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-16 13:16:31 +0500

    Add files via upload

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Lab06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.listBoxItems = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(218, 302);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(91, 32);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(364, 302);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(91, 32);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(507, 302);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(91, 32);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(438, 377);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(91, 32);
+            this.btnDisplay.TabIndex = 3;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // listBoxItems
+            // 
+            this.listBoxItems.FormattingEnabled = true;
+            this.listBoxItems.Location = new System.Drawing.Point(364, 139);
+            this.listBoxItems.Name = "listBoxItems";
+            this.listBoxItems.Size = new System.Drawing.Size(242, 95);
+            this.listBoxItems.TabIndex = 4;
+            this.listBoxItems.SelectedIndexChanged += new System.EventHandler(this.listBoxItems_SelectedIndexChanged);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(364, 63);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(242, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Items: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(171, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "List: ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(650, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.listBoxItems);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.ListBox listBoxItems;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab06
+{
+    public partial class Form1 : Form
+    {
+        // Declare the list to store data
+        private List<string> dataList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string inputData = txtInput.Text.Trim();
+            if (!string.IsNullOrEmpty(inputData))
+            {
+                if (!dataList.Contains(inputData))
+                {
+                    dataList.Add(inputData);
+                    UpdateListBox(); // Update ListBox with numbering
+                    MessageBox.Show($"'{inputData}' has been added to the list.",
+                        "Information",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"'{inputData}' is already in the list.",
+                        "Information",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+                txtInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter an item to add.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string inputData = txtInput.Text.Trim();
+            if (dataList.Contains(inputData))
+            {
+                dataList.Remove(inputData);
+                UpdateListBox(); // Update ListBox with numbering
+                MessageBox.Show($"'{inputData}' has been removed from the list.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                txtInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show($"'{inputData}' was not found in the list.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string inputData = txtInput.Text.Trim();
+            if (dataList.Contains(inputData))
+            {
+                MessageBox.Show($"'{inputData}' exists in the list.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                // Highlight the item in the ListBox
+                int index = dataList.IndexOf(inputData);
+                if (index >= 0)
+                {
+                    listBoxItems.SetSelected(index, true);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"'{inputData}' does not exist in the list.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            // Ensure the ListBox is updated with the latest dataList
+            UpdateListBox();
+
+            // Check if there are any items in the dataList
+            if (dataList.Count > 0)
+            {
+                // Create a string to hold all list items to be displayed in the message box
+                StringBuilder listItems = new StringBuilder();
+
+                // Append each item in the list to the string
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    listItems.AppendLine($"{i + 1}. {dataList[i]}"); // Add numbered item
+                }
+
+                // Show the entire list in a message box
+                var result = MessageBox.Show(listItems.ToString(),
+                    "Complete List of Items",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Information);
+
+                // Handle the OK/Cancel result if necessary
+                if (result == DialogResult.OK)
+                {
+                    // Handle the case when OK is clicked
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("The list is empty.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void listBoxItems_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBoxItems.SelectedItem != null)
+            {
+                int selectedIndex = listBoxItems.SelectedIndex;
+                if (selectedIndex >= 0)
+                {
+                    string selectedItem = dataList[selectedIndex];
+                    dataList.RemoveAt(selectedIndex); // Remove from the List
+                    UpdateListBox(); // Update ListBox with numbering
+                    MessageBox.Show($"'{selectedItem}' has been removed from the list.",
+                        "Information",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No item selected.",
+                    "Information",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataList.Clear(); // Clear the List
+            listBoxItems.Items.Clear(); // Clear the ListBox
+            MessageBox.Show("The list has been cleared.",
+                "Information",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+      
+        private void UpdateListBox()
+        {
+            listBoxItems.Items.Clear(); // Clear existing items in the ListBox
+            if (dataList.Count > 0)
+            {
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    listBoxItems.Items.Add($"{i + 1}. {dataList[i]}");
+                }
+            }
+            else
+            {
+                listBoxItems.Items.Add("No items in the list");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataList.Clear(); // Clear the List
+            listBoxItems.Items.Clear(); // Clear the ListBox
+            MessageBox.Show("The list has been cleared.",
+                "Information",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void listBoxItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
