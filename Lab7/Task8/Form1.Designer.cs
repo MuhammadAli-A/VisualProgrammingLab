d9b64c878e5c78614bf0b115ae0176e2ea0d1899
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-20 18:41:14 +0500

    Add files via upload

---
+++ Lab7/Task8/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Task8
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.Label labelImageIndex;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonNext;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.labelImageIndex = new System.Windows.Forms.Label();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(12, 12);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(100, 30);
+            this.buttonBrowse.TabIndex = 0;
+            this.buttonBrowse.Text = "Browse Folder";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Location = new System.Drawing.Point(12, 50);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(760, 500);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPreview.TabIndex = 1;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // labelImageIndex
+            // 
+            this.labelImageIndex.AutoSize = true;
+            this.labelImageIndex.Location = new System.Drawing.Point(650, 12);
+            this.labelImageIndex.Name = "labelImageIndex";
+            this.labelImageIndex.Size = new System.Drawing.Size(49, 17);
+            this.labelImageIndex.TabIndex = 2;
+            this.labelImageIndex.Text = "0 / 0";
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(12, 560);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 30);
+            this.buttonPrevious.TabIndex = 3;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(93, 560);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 30);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 611);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.labelImageIndex);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.buttonBrowse);
+            this.Name = "Form1";
+            this.Text = "Photo Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Lab7/Task8/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task8
+{
+    public partial class Form1 : Form
+    {
+        private string[] imageFiles; // Array to hold image file paths
+        private int currentIndex = 0; // To keep track of the current image
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Browse folder button click event
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            // Open Folder Browser dialog to select folder
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get all image files in the selected folder
+                    string folderPath = folderDialog.SelectedPath;
+                    imageFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly);
+
+                    // Filter to only image files
+                    imageFiles = Array.FindAll(imageFiles, file => file.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                                                  file.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                                                                  file.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                                                                  file.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase));
+
+                    if (imageFiles.Length > 0)
+                    {
+                        currentIndex = 0; // Reset to the first image
+                        ShowImage();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No images found in the selected folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        // Show image in PictureBox
+        private void ShowImage()
+        {
+            if (imageFiles.Length > 0 && currentIndex >= 0 && currentIndex < imageFiles.Length)
+            {
+                // Load the image into PictureBox
+                pictureBoxPreview.ImageLocation = imageFiles[currentIndex];
+                labelImageIndex.Text = $"{currentIndex + 1} / {imageFiles.Length}";
+            }
+        }
+
+        // Previous button click event
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                ShowImage();
+            }
+        }
+
+        // Next button click event
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (currentIndex < imageFiles.Length - 1)
+            {
+                currentIndex++;
+                ShowImage();
+            }
+        }
+    }
+}
