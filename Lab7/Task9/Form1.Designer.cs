cfbb8be4c9c17b3a99c9a07e854ebd91f19e206d
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-20 18:41:48 +0500

    Add files via upload

---
+++ Lab7/Task9/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Task9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelCharacterCount;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelCharacterCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.textBoxInput.MaxLength = 160;
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(449, 100);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // labelCharacterCount
+            // 
+            this.labelCharacterCount.AutoSize = true;
+            this.labelCharacterCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCharacterCount.Location = new System.Drawing.Point(12, 120);
+            this.labelCharacterCount.Name = "labelCharacterCount";
+            this.labelCharacterCount.Size = new System.Drawing.Size(168, 24);
+            this.labelCharacterCount.TabIndex = 1;
+            this.labelCharacterCount.Text = "Characters left: 160";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(473, 181);
+            this.Controls.Add(this.labelCharacterCount);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Character Limit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Lab7/Task9/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for TextChanged event of the TextBox
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            // Calculate the number of characters left
+            int remainingCharacters = 160 - textBoxInput.Text.Length;
+
+            // Update the label to show the remaining characters
+            labelCharacterCount.Text = $"Characters left: {remainingCharacters}";
+        }
+    }
+}
