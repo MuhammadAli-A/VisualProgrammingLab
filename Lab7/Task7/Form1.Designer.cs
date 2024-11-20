30550f53ff3f8046b8379cfa1ad014c342a1b56c
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-20 18:25:44 +0500

    Add files via upload

---
+++ Lab7/Task7/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Task7
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Timer clockTimer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelClock = new System.Windows.Forms.Label();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClock.Location = new System.Drawing.Point(62, 87);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(61, 37);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "\"  \"";
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(625, 334);
+            this.Controls.Add(this.labelClock);
+            this.Name = "Form1";
+            this.Text = "Wall Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Lab7/Task7/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the label with the current date and time in the desired format
+            labelClock.Text = DateTime.Now.ToString("dddd, MMMM dd, yyyy" + Environment.NewLine + "hh:mm:ss tt");
+        }
+
+        // Event handler for Timer Tick
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            // Update the label with the current date and time in the desired format
+            labelClock.Text = DateTime.Now.ToString("dddd, MMMM dd, yyyy" + Environment.NewLine + "hh:mm:ss tt");
+        }
+
+        // Additional Form Load logic (if needed)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Optional: Any additional initialization can go here
+        }
+    }
+}
