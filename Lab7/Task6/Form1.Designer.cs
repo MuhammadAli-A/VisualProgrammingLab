a0128d42c5db43ccea14ec3407b36541a71f6779
MuhammadAli-A <233510@students.au.edu.pk>
2024-11-20 18:18:23 +0500

    Add files via upload

---
+++ Lab7/Task6/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Task6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCountdown = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTime.Location = new System.Drawing.Point(189, 36);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(168, 29);
+            this.textBoxTime.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(231, 84);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 31);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.TimerCountdown_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Set Time :";
+            // 
+            // labelCountdown
+            // 
+            this.labelCountdown.AutoSize = true;
+            this.labelCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCountdown.Location = new System.Drawing.Point(56, 138);
+            this.labelCountdown.Name = "labelCountdown";
+            this.labelCountdown.Size = new System.Drawing.Size(159, 24);
+            this.labelCountdown.TabIndex = 3;
+            this.labelCountdown.Text = "Remaining Time :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.labelCountdown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxTime);
+            this.Name = "Form1";
+            this.Text = "Time Span";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCountdown;
+    }
+}
+
---
+++ Lab7/Task6/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task6
+{
+    public partial class Form1 : Form
+    {
+        private int remainingTime; // Variable to store the remaining time in seconds
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000; // Set the timer interval to 1 second (1000 ms)
+            timer.Tick += TimerCountdown_Tick; // Event handler for Timer tick
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Any initialization code you want when the form loads
+        }
+
+
+
+        // Event handler for the timer's Tick event
+        private void TimerCountdown_Tick(object sender, EventArgs e)
+        {
+            // Decrease the remaining time by 1 second
+            remainingTime--;
+
+            // Update the label with the new remaining time
+            labelCountdown.Text = remainingTime.ToString();
+
+            // If the countdown reaches zero
+            if (remainingTime == 0)
+            {
+                // Stop the timer
+                timer.Stop();
+
+                // Show a message when the time is over
+                MessageBox.Show("Time's up!", "Countdown Finished", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Event handler for the "Start" button click
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Check if the input is a valid number
+            if (int.TryParse(textBoxTime.Text, out remainingTime) && remainingTime > 0)
+            {
+                // Display the countdown time in the label
+                labelCountdown.Text = remainingTime.ToString();
+
+                // Start the countdown timer
+                timer.Start();
+            }
+            else
+            {
+                // Show an error message if the input is invalid
+                MessageBox.Show("Please enter a valid number greater than 0.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Optional: Event handler for the "Exit" button if you want to close the application
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
