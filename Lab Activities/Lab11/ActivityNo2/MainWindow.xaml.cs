712a95dd5fa0ed6d5012b45a740cead4ac3b8281
MuhammadAli-A <233510@students.au.edu.pk>
2024-12-16 21:05:12 +0500

    Add files via upload

---
+++ Lab Activities/Lab11/ActivityNo2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFCheckBoxControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Name == "checkBox1")
+                textBox1.Text = "2 state CheckBox is checked.";
+            else
+                textBox2.Text = "3 state CheckBox is checked.";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Name == "checkBox1")
+                textBox1.Text = "2 state CheckBox is unchecked.";
+            else
+                textBox2.Text = "3 state CheckBox is unchecked.";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "3 state CheckBox is in indeterminate state.";
+        }
+    }
+}
---
+++ Lab Activities/Lab11/ActivityNo3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabNo10_ActivityNo3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo1_SelectChange(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox1.Text = selectedItem.Content.ToString();
+            }
+            else
+            {
+                textBox1.Text = string.Empty; 
+            }
+        }
+        private void Combo2_SelectChange(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox2.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox2.Text = selectedItem.Content.ToString();
+            }
+            else
+            {
+                textBox2.Text = string.Empty; 
+            }
+        }
+    }
+}
